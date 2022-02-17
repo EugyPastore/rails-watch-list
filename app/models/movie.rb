35075ddd3ccdb58830d571540e9fa7d1af41402c
Eugenia Pastore <eugenia.pastore@gmail.com>
2022-02-17 17:26:49 +0100

    created methods for list, added bookmark method

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,8 +2,4 @@ class Movie < ApplicationRecord
   validates :title, uniqueness: true, presence: true
   validates :overview, uniqueness: true, presence: true
   has_many :bookmarks
-
 end
-
-
-#You can’t delete a movie if it is referenced in at least one bookmark.
